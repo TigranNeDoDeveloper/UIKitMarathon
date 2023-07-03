705139d13eca9a84323e5fba2e6e5da0bbe4d6e4
Тигран Левонян <levonyan.t@bk.ru>
2023-07-03 18:32:33 +0300

    homework done

--- UIKitMarathon/UIKitMarathon/ViewController.swift
+++ UIKitMarathon/UIKitMarathon/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    private lazy var rectangleView: GradientView = {
+        let view = GradientView()
+        view.cornerRadius = 16
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
+    private func setupUI() {
+        view.backgroundColor = .white
+        view.addSubview(rectangleView)
 
+        NSLayoutConstraint.activate([
+            rectangleView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            rectangleView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+            rectangleView.widthAnchor.constraint(equalToConstant: 100),
+            rectangleView.heightAnchor.constraint(equalTo: rectangleView.widthAnchor)
+        ])
+    }
 }
 
